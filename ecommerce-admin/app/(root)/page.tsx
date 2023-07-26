2f2e7256b12a24847d46a677a010d1faff190fc7
nleung77 <nleung77@gmail.com>
2023-07-26 13:51:31 -0400

    updated main page.tsx

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+const SetupPage = () => {
   return (
     <div className="p-4">
       <Button>Click Me</Button>
